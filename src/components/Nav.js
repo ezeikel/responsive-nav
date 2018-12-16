909d94bc40fa4ef27b12c1616aedd3b885ce6c3e
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-12-16 04:24:26 +0000

    Got rid of Hamburger. Nav on the bottom on mobile.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,38 +1,21 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import Hamburger from './Hamburger';
 
 const Wrapper = styled.nav`
-  @media (min-width: 1024px) {
-  }
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  box-shadow: 0 -3px 3rem 0px rgba(0, 0, 0, 0.1);
 `;
 
 const NavLinks = styled.ul`
-  position: absolute;
-  background: tomato;
-  right: 0;
-  width: 320px;
-  height: 100vh;
-  transform: ${({ active }) => `translateX(${active ? 0 : '100vw'})`};
-  transition: transform 0.3s ease-in-out;
+  //transform: ${({ active }) => `translateX(${active ? 0 : "100vw"})`};
+  //transition: transform 0.3s ease-in-out;
+  display: flex;
+  justify-content: space-between;
+  padding: var(--spacing-medium);
   @media (min-width: 1024px) {
-    position: static;
-    height: auto;
-    display: flex;
-    flex-direction: row;
-    li + li {
-      margin-left: var(--spacing-medium);
-    }
-  }
-`;
-
-const StyledHamburger = styled(Hamburger)`
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  @media (min-width: 1024px) {
-    display: none;
   }
 `;
 
@@ -42,12 +25,11 @@ const Nav = () => {
   return (
     <Wrapper>
       <NavLinks active={active}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="blog">Blog</Link></li>
-        <li><Link to="about">About</Link></li>
-        <li><Link to="contact">Contact</Link></li>
+        <li><Link to="/">Account</Link></li>
+        <li><Link to="blog">Saved</Link></li>
+        <li><Link to="about">Messages</Link></li>
+        <li><Link to="contact">Cart</Link></li>
       </NavLinks>
-      <StyledHamburger active={active} toggleActive={toggleActive} />
     </Wrapper>
   );
 };
