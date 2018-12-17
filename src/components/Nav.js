833d49454acdfbfc8165623989defd1650d6872b
Ezeikel Pemberton <ezeikel_pemberton@discovery.com>
2018-12-17 15:37:08 +0000

    Fixed nav not being 100% width and fixed on mobile.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Wrapper = styled.nav`
   position: fixed;
   bottom: 0;
-  width: 100%;
+  left: 0;
+  right: 0;
   box-shadow: 0 -3px 3rem 0px rgba(0, 0, 0, 0.1);
   @media (min-width: 768px) {
     position: relative;
